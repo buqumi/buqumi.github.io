87c97c8cfab90a1e1f19c9453609f4b2b3ae57b4
buqumi <quangminhbui10031992@gmail.com>
2018-07-22 01:05:30 +0700

    sua lai api

--- src/assets/scripts/nasa.js
+++ src/assets/scripts/nasa.js
@@ -1,21 +1,20 @@
-
 // nhan vao: string ngay thang va so ngay thay doi || tra ve: object ngay thang time
-function addDays(dateString, days){
+function addDays(dateString, days) {
     var dateObject123 = new Date(dateString)
     dateObject123.setDate(dateObject123.getDate() + days)
     return dateObject123
 }
 
 // nhan vao: object ngay thang time || tra ve string ngay thang ko co' time
-function dateWithoutTime(dateObject){
+function dateWithoutTime(dateObject) {
     var newDateWithoutTime = dateObject.toISOString().split('T')[0]
     return newDateWithoutTime //string
 }
 
 // nhan vao string ngay thang va so ngay thay doi || tra ve: string duong link + ngay thang da +/- khong co' time
-function tinhNgay(dateStr,soNgayThayDoi){
+function tinhNgay(dateStr, soNgayThayDoi) {
     //tinh ngay can hien thi
-    var newdateObject = addDays(dateStr,soNgayThayDoi)
+    var newdateObject = addDays(dateStr, soNgayThayDoi)
     // console.log('newdateObject:',newdateObject)
     //cat thoi gian
     var dateWithoutTimeStr = dateWithoutTime(newdateObject)
@@ -23,76 +22,140 @@ function tinhNgay(dateStr,soNgayThayDoi){
     // console.log('dateWithoutTimeStr',dateWithoutTimeStr)
 
     //tra ve 1 chuoi moi gom link + ngay can hien thi da cat thoi gian
-    var chuoiTam = 'https://api.nasa.gov/planetary/apod?api_key=nAWtjI3JNh998ZEzaXj1jLJbeO8AGxBOSoXGlNYx&date='+ dateWithoutTimeStr
+    var chuoiTam = 'https://api.nasa.gov/planetary/apod?api_key=nAWtjI3JNh998ZEzaXj1jLJbeO8AGxBOSoXGlNYx&date=' + dateWithoutTimeStr
     // return chuoiTam
     return chuoiTam
 }
 
 // var chuoiMoi = tinhNgay('2018-03-12', -2) // Tao muon chuoiMoi === https://api.nasa.gov/planetary/apod?api_key=nAWtjI3JNh998ZEzaXj1jLJbeO8AGxBOSoXGlNYx&date=2018-03-13
-var chuoiMoi = tinhNgay('2018-03-12', 1)
+// var chuoiMoi = tinhNgay('2018-03-12', 1)
+
+function next_nasa() {
+    console.log('next')
+}
+
+function pre_nasa() {
+    console.log('pre')
+}
+
+// Vue.component('the-bai-bao-nasa', {
+//     props: ['viewinfo'],
+//     template: `
+//     <div>
+//         <div id="titlenasa">
+//             <h1>{{viewinfo.title}}</h1>
+//         </div>
+//         <div id="headernasa">
+//             <img v-bind:src="viewinfo.url">
+//             <div class="copyright">
+//                 <h2>{{viewinfo.copyright}}</h2>
+//             </div>
+//         </div>
+
+//         <div id="contentnasa">
+//             <h3>{{viewinfo.explanation}}</h3>
+//             <div id="datetime">
+//                 <h3>{{viewinfo.date}}</h3>
+//             </div>
+//         </div>
+//     </div>
+// `
+// })
+
 
 var app_nasa = new Vue({
     el: '#vue_app',
     data: {
-        message: '', 
-        NasaData: []
+        message: '',
+        NasaData: '',
+        tempDate: '',
+        disableNextBtn: false
     },
-    created(){
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=nAWtjI3JNh998ZEzaXj1jLJbeO8AGxBOSoXGlNYx&date=2018-03-13')
-        .then(function(response){
-            var arrayNasas = response.data
-            var dataObjs = [];
-            for(i=0;i<arrayNasas.length;i++){
-                if(chuoiMoi=chuoiMoi++){
-                    var dataObj = {
-                        id = i,
-                        title: arrayNasas[i].title,
-                        url: arrayNasas[i].url,
-                        copyright: arrayNasas[i].copyright,
-                        explanation: arrayNasas[i].explanation,
-                        dateStr:arrayNasas[i].date
-                    }
-                    dataObjs.push(dataObj);
-                    document.getElementById("nextnasa").onclick=function(){
-                        response()
-                    }
-                    function response(){
-                        document.getElementById("nextnasa").innerHTML=dataObjs;
-                    }
-                }
+    methods: {
+        load: function () {
+        
+            var baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=nAWtjI3JNh998ZEzaXj1jLJbeO8AGxBOSoXGlNYx&date='
+
+            var today = new Date()
+            var todayIso = today.toISOString()
+            var todayIsoNoTime = todayIso.split('T')[0]
+
+            var fullUrl = baseUrl + todayIsoNoTime
+
+            axios.get(fullUrl)
+                .then(function (res) {
+                    this.NasaData = res.data
+                }.bind(this))
+                .catch(function(err){
+                    console.log(err)
+                })
+
+            this.tempDate = todayIsoNoTime
+            this.checkNextBtn()            
+        },
+        nextNasa: function () {
+            var baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=nAWtjI3JNh998ZEzaXj1jLJbeO8AGxBOSoXGlNYx&date='
+
+            var addDay = addDays(this.tempDate, 1)
+            var outTime = dateWithoutTime(addDay)
+
+            var fullUrl = baseUrl + outTime
+            console.log(fullUrl)
+
+            axios.get(fullUrl)
+                .then(function (res) {
+                    this.NasaData = res.data
+                }.bind(this))
+                .catch(function(err){
+                    console.log(err)
+                })
+
+            this.tempDate = outTime
+            this.checkNextBtn()
+        },
+        preNasa: function () {
+            var baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=nAWtjI3JNh998ZEzaXj1jLJbeO8AGxBOSoXGlNYx&date='
+
+            var minusDay = addDays(this.tempDate, -1)
+            var outTime = dateWithoutTime(minusDay)
+
+            var fullUrl = baseUrl + outTime
+            console.log(fullUrl)
+
+            axios.get(fullUrl)
+                .then(function (res) {
+                    this.NasaData = res.data
+                }.bind(this))
+                .catch(function(err){
+                    console.log(err)
+                })
+            
+            this.tempDate= outTime
+            this.checkNextBtn()
+        },
+        checkNextBtn: function () {
+            var dateObj = new Date (this.tempDate)
+
+            var toDay = new Date()
+            toDay.setHours(0,0,0,0)//set toDay ve 0h,0m,0s,0ms
+
+            if(dateObj<toDay){
+                this.disableNextBtn = false
+            } else {
+                this.disableNextBtn = true
             }
-        )}
+        }
+    },
+    created()//noi goi function luc no moi load
+     {
+        this.load()
     }
 })
-        
 
 
 
-Vue.component('the-hinh-anh',{
-    props:['viewinfo'],
-    template:`
 
-    <div id="titlenasa">
-        <h1>{{viewinfo.title}}</h1>
-    </div>
 
-    <div id="headernasa">
-        <img v-bind:src="viewinfo.url">
-        <div class="copyright">
-            <h2>{{viewinfo.copyright}}</h2>
-        </div>
-    </div>
-
-    <div id="contentnasa">
-        <h3>{{viewinfo.explanation}}</h3>
-        <div id="datetime">
-            <h3>{{dateStr}}</h3>
-        </div>
-    </div>
-    <button class="prenasa">Previous</button>
-    <button class="nextnasa">Next</button>
-`
-})
 
 
 
@@ -131,8 +194,10 @@ Vue.component('the-hinh-anh',{
 //             var datetoday = datetime;
 
 //             for(i=0;i<arrayData.length;i++){
-               
+
 //             }
 //         })
 //     }
 // })
+
+{ /*  */ }
