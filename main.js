d69a95fac75e1b745e4d0588649b648c4fe6ed20
buqumi <quangminhbui10031992@gmail.com>
2018-09-03 15:42:21 +0700

    VietDS

---
+++ main.js
@@ -0,0 +1,6 @@
+window.sr=ScrollReveal();
+sr.reveal('.VDSlogo',{
+    duration:2000,
+    origin:'top',
+    distance:'300px'
+});
--- src/vidu.js
+++
@@ -1,3 +0,0 @@
-var array = [3]
-array[4]=0
-console.log('array = ',array.length)
